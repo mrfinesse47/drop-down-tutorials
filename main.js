5cdb1fe141b414d8b5af309cbc2a2d1f2f786c30
Kevin Mason <krmason2020@gmail.com>
2022-03-15 22:12:09 -0700

    able to close the menu on an off click when it is open

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 document.getElementById("menu").addEventListener("click", openMenu);
-
-function openMenu() {
+window.addEventListener("click", () => {
   document.getElementById("drop-down").classList.toggle("active");
+});
+
+function openMenu(event) {
+  document.getElementById("drop-down").classList.toggle("active");
+  event.stopPropagation(); //this is to not have the click on the drop down fire the window event for click
 }
